5c9857c9f087d3ba91c471fa122d156a8ccd8311
marialu11 <mgaleano@unal.edu.co>
2024-03-22 11:32:05 -0500

    add: cors

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 import librosRoutes from './routes/libros.routes.js';
 import autoresRoutes from './routes/autores.routes.js';
 
@@ -8,6 +9,7 @@ const app = express();
 //Para que el servidor entienda los datos que llegan del cliente en formato JSON
 app.use(express.json())
 
+app.use(cors())
 app.use(librosRoutes)
 app.use(autoresRoutes)
 
