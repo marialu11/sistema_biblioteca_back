dc60c021de38e46a30020b9a8c5671c2b3d50960
marialu11 <mgaleano@unal.edu.co>
2024-03-22 09:14:09 -0500

    add: controler to create and get libros

--- src/controllers/libros.controller.js
+++ src/controllers/libros.controller.js
@@ -1,8 +1,45 @@
-export const getLibros = (req, res) => {
-    res.send('Trayendo libros')
+import {libro} from '../models/libro.js'
+
+export const getLibros = async(req, res) => {
+    try {
+        const libros = await libro.findAll()
+        res.json(libros)
+    }
+    catch (error) {
+        return res.status(500).json({
+            message: error.message
+        })
+    }
 }
 
-export const createLibros = (req, res) => {
-    console.log(req.body)
-    res.send('Creando libros')
+export const createLibros = async (req, res) => {
+    const { libro_isbn, libro_titulo, fk_autor_id } = req.body
+    try {
+        const newLibro = await libro.create({
+            libro_isbn,
+            libro_titulo,
+            fk_autor_id
+        })
+        res.json(newLibro)
+    } catch (error) {
+        return res.status(500).json({
+            message: error.message
+        })
+    }
+}
+
+export const createAutores = async (req, res) => {
+    const { autor_nombre, autor_apellido } = req.body
+
+    try {
+        const newAutor = await autor.create({
+            autor_nombre,
+            autor_apellido
+        })
+        res.json(newAutor)
+    } catch (error) {
+        return res.status(500).json({
+            message: error.message
+        })
+    }
 }
